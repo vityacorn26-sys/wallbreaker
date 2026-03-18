85278ad4d4017ff6c385834ac282dbd4b796ec07
vityacorn26-sys <vitya.corn26@gmail.com>
2026-03-18 12:28:50 +0200

    Create config.js

---
+++ client/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    // ЗАМЕНИ 'ТВОЙ_IP' НА РЕАЛЬНЫЙ IP СВОЕГО СЕРВЕРА
+    API_URL: 'http://75.119.147.2:3000', 
+    
+    BOT_LINK: 'https://t.me/BypassWallBot',
+    OWNER_ID: '7614360974', 
+    PROXY_BOT_LINK: 'https://t.me/hiddifyProxySale_bot',
+    ADSGRAM_BLOCK_ID: '1234' // Тестовый ID
+};
