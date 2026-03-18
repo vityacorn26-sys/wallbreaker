a1960e1ad5be52b789c6d5d044a298d73afab870
vityacorn26-sys <vitya.corn26@gmail.com>
2026-03-18 14:28:31 +0200

    Update config.js

--- client/config.js
+++ client/config.js
@@ -1,9 +1,22 @@
 const CONFIG = {
-    // ЗАМЕНИ 'ТВОЙ_IP' НА РЕАЛЬНЫЙ IP СВОЕГО СЕРВЕРА
-    API_URL: 'http://75.119.147.2:3000', 
+    // Твой защищенный сервер
+    API_URL: 'https://setgot.qzz.io:3000',
     
+    // Ссылки на твои ресурсы
     BOT_LINK: 'https://t.me/BypassWallBot',
-    OWNER_ID: '7614360974', 
+    OWNER_ID: '7614360974',
     PROXY_BOT_LINK: 'https://t.me/hiddifyProxySale_bot',
-    ADSGRAM_BLOCK_ID: '1234' // Тестовый ID
+    
+    // Настройки для будущей рекламы (Adsgram)
+    ADSGRAM_BLOCK_ID: '1234', // Пока тестовый ID
+    
+    // Настройки игры
+    MAX_ENERGY: 100,
+    REGEN_SPEED: 1000, // 1 единица энергии в секунду
+    
+    // Тексты (для будущего переключателя)
+    LANG: {
+        RU: { tap: "ТАПАЙ!", balance: "Баланс" },
+        EN: { tap: "TAP!", balance: "Balance" }
+    }
 };
