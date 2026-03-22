061135cf6949fc8861b106f3708a506710095c31
vityacorn26-sys <vitya.corn26@gmail.com>
2026-03-22 17:17:29 +0200

    Update api.js

--- client/js/api.js
+++ client/js/api.js
@@ -1,18 +1,23 @@
 const API = {
+    // Укажи здесь свой поддомен точно!
+    BASE_URL: 'https://api.setgot.qzz.io', 
+
     async getUser(id) {
-        const res = await fetch(`${CONFIG.API_BASE_URL}/api/user?id=${id}`);
+        const res = await fetch(`${this.BASE_URL}/api/user?id=${id}`);
         return await res.json();
     },
+
     async sendTap(id) {
-        const res = await fetch(`${CONFIG.API_BASE_URL}/api/tap`, {
+        const res = await fetch(`${this.BASE_URL}/api/tap`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({ id })
         });
         return await res.json();
     },
+
     async claimAdReward(id) {
-        const res = await fetch(`${CONFIG.API_BASE_URL}/api/ad-reward`, {
+        const res = await fetch(`${this.BASE_URL}/api/ad-reward`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({ id })
