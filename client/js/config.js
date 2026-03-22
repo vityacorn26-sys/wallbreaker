dabd7d58a167ed0f40f384cd5bbf27fad53acdfb
vityacorn26-sys <vitya.corn26@gmail.com>
2026-03-22 14:37:46 +0200

    Update config.js

--- client/js/config.js
+++ client/js/config.js
@@ -1,18 +1,12 @@
 const CONFIG = {
-    // Твой основной домен для связи с сервером через Cloudflare
-    API_URL: "https://api.setgot.qzz.io", 
-    
-    // Тестовый ID Adsgram (заменишь на свой из бота, когда получишь)
-    ADSGRAM_BLOCK_ID: "24607", 
-    
-    // Наше новое название валюты
-    CURRENCY_NAME: "WBC",
-    
-    // Лимит, о котором договаривались: 3 тапа в секунду
-    TAPS_PER_SECOND_LIMIT: 3,
-
-    // Настройки энергии и рангов для логики игры
-    BASE_TAP_VALUE: 10,
-    MAX_ENERGY: 100,
-    REGEN_SPEED: 30000 // 30 секунд на 1 единицу
+    API_BASE_URL: "https://api.setgot.qzz.io",
+    ADSGRAM_BLOCK_ID: "24607",
+    MY_TON_WALLET: "UQC_ZtfJoSzLPMCR7SdfjCI6-A5jOZ5VNbLPWjw99fvqreGj", // Вставь свой адрес
+    RANKS: {
+        1: { name: "Newbie", mult: 10, img: "assets/cat1.jpg" },
+        2: { name: "Pro", mult: 25, img: "assets/cat2.jpg" },
+        3: { name: "Punk", mult: 60, img: "assets/cat3.jpg" }, // 0.5 TON
+        4: { name: "Agent", mult: 150, img: "assets/cat4.jpg" },
+        5: { name: "CyberGod", mult: 400, img: "assets/cat5.jpg" }
+    }
 };
