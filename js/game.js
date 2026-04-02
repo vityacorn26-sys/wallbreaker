f08b7f75fb702ff54acc456feb02555d48b13ec1
vityacorn26-sys <vitya.corn26@gmail.com>
2026-04-02 21:57:18 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -397,7 +397,7 @@ function getTonWalletShort() {
 
   if (!raw) return "";
 
-  return `${appName}: ${String(raw).slice(0, 6)}...${String(raw).slice(-6)}`;
+  return `${appName}: ${String(raw)}`;
 }
 
 function updateAccountPanel() {
